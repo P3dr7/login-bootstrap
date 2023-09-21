10d01df68770b7ef647010dbfc690ff2f7adabae
Pedro Ferri <pedroferri@edu.unifil.br>
2023-09-21 14:31:34 -0300

    Funcionalidades a fazer

--- app/cadastro.php
+++ app/cadastro.php
@@ -6,6 +6,7 @@ $filename = "../files/arquivo-leitura.csv";
 $file=fopen($filename, "w+"); 
 $headers = fgets($file);
 
-
-
+/* fseac
+usar funcao que coloca no final antes de rodar o for
+colocar o arquivo em memoria, apagar ele reescrever e dps upar de volta"*/
 ?>
