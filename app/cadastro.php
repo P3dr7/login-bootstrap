f8661da65479e7fd86e8a5dd91b61f38ce32955a
Pedro Ferri <pedroferri@edu.unifil.br>
2023-09-22 13:44:30 -0300

    Testando for

--- app/cadastro.php
+++ app/cadastro.php
@@ -1,10 +1,31 @@
 <?php
 
-$nome = $_POST['nome'];
+$nome = $_POST['e-mail'];
 $senha = md5($_POST['password']);
 $filename = "../files/arquivo-leitura.csv";
-$file=fopen($filename, "w+"); 
-$headers = fgets($file);
+
+
+$headers = array();
+$date = array();
+
+
+$file=fopen($filename, "a+"); 
+//fwrite($file, implode(",", $headers). "\r\n");
+
+
+if(isset($senha)){
+    //$headers = explode(",", fgets($file));
+    $concat = $nome . "," . $senha;
+    if ($file){
+        for ($i=0; $i < 2; $i++) { 
+            $concat = $i . "," . $concat;
+            array_push($data, $concat);
+            fwrite($file , implode(",", $data)."\r\n");
+        }
+        
+    }
+}
+fclose($file);
 
 /* fseac
 usar funcao que coloca no final antes de rodar o for
