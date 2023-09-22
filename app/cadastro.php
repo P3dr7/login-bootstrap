9970e3b2e4af4bbdfd1a8f40a4b4ad40ed1160a2
Pedro Ferri <pedroferri@edu.unifil.br>
2023-09-22 15:08:51 -0300

    Funcionalidade de cadastro no CSV Concluida

--- app/cadastro.php
+++ app/cadastro.php
@@ -1,39 +1,39 @@
 <?php
-
 $nome = $_POST['e-mail'];
 $senha = md5($_POST['password']);
 $filename = "../files/arquivo-leitura.csv";
-$file=fopen($filename, "a+"); 
-$headers = fgets($file);
 
-
-if(isset($senha)){
-    //$headers = explode(",", fgets($file));
-    $concat = $nome . "," . $senha;
-    $data = fgets($file);
-    if ($file){
-        //Verifica todas as linhas do arquivo
-        while (($lineFile = fgetcsv($file)) !== FALSE){
-            $dataFile[] = $lineFile; // Armazena cada linha em uma matriz
-        }
-    
-        // Conta a quantidade de colunas do array na posição 0 
-        
-        $numColumns = count($dataFile[0]);
-    
-        // Imprime a quantidade colunas. 
-        echo $numColumns;
-       
-        for ($i=0; $i < $numColumns; $i++) { 
-            $concat = $i. "," . $concat;
-
-        }
-        fwrite($file , $concat."\r\n");
-        
-    }  
+// Abra o arquivo CSV para leitura e escrita
+$file = fopen($filename, "a+");
+if ($file === false) {
+    die("Não foi possível abrir o arquivo.");
 }
-fclose($file);
-/* fseac
-usar funcao que coloca no final antes de rodar o for
-colocar o arquivo em memoria, apagar ele reescrever e dps upar de volta"*/
+
+// Bloqueie o arquivo para evitar problemas de concorrência
+if (flock($file, LOCK_EX)) {
+    // Conte o número de linhas existentes no arquivo
+    $lineCount = 0;
+    while (fgets($file)) {
+        $lineCount++;
+    }
+
+    // Incrementar o contador de linhas (o primeiro campo, neste caso, ID)
+    $lineCount++;
+
+    // Crie o novo registro com o ID incrementado e os dados fornecidos
+    $newRecord = [$lineCount, $nome, $senha];
+
+    // Escreva o novo registro no arquivo CSV
+    fputcsv($file, $newRecord);
+
+    // Solte o bloqueio do arquivo
+    flock($file, LOCK_UN);
+
+    // Feche o arquivo
+    fclose($file);
+
+    echo "O novo registro foi adicionado ao CSV com sucesso.";
+} else {
+    echo "Não foi possível bloquear o arquivo para escrita.";
+}
 ?>
