3c8027a83249a3fd0ac394da7d3f37dca427d23c
Pedro Ferri <pedroferri@edu.unifil.br>
2023-09-25 17:03:02 -0300

    Finalizei a pagina de login e a verificacao Foi finalizado a pagina de login, onde os dados sao encaminhados para a 'verificaLogin.php' e la retorna se foi validado ou nao

--- app/verificaLogin.php
+++ app/verificaLogin.php
@@ -1,3 +1,47 @@
 <?php
+//Variaveis
 $emailLogin = $_POST['e-mail'];
 $senhaLogin = $_POST['password'];
+$filename = "../files/arquivo-leitura.csv";
+$data = array();
+
+//transforma os dados do arquivo em um array 
+if(file_exists($filename)) {
+    $file = fopen($filename,"r");
+
+    //Le o cabecalho
+    $headers = fgetcsv($file);
+    
+    //le o resto das linhas
+    while ($row = fgetcsv($file)) {
+        if (count($headers) == count($row)) {
+            $data[] = array_combine($headers, $row); // Combina cabeçalhos e valores
+        } else {
+            // Para depuração: imprime os dados problemáticos
+            echo "Erro na linha: ";
+            print_r($row);
+            echo "<br>";
+        }
+    }
+    fclose($file);
+}else{
+    die ("Por favor cadastre-se Primeiro");
+}
+// Verifica dados 
+$userFound = false;
+//roda os dados da tabela como array comparando cada campo
+foreach ($data as $userData) {
+    if ($userData['Email'] == $emailLogin && $userData['Senha(md5)'] == md5($senhaLogin)) {
+        $userFound = true;
+        break;
+    }
+}
+
+if ($userFound) {
+    echo "Usuário logado com sucesso";
+} else {
+    echo "Email ou senha inválidos";
+}
+
+
+?>
